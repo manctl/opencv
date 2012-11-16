5bb2c0f5bd00faa01a26ef8e20bbacf89ac4fc48
Nicolas Tisserand <nt@manctl.com>
2012-11-16 08:39:11 +0100

    Fix macosx install names.

--- cmake/OpenCVModule.cmake
+++ cmake/OpenCVModule.cmake
@@ -457,7 +457,7 @@ macro(ocv_create_module)
     DEBUG_POSTFIX "${OPENCV_DEBUG_POSTFIX}"
     ARCHIVE_OUTPUT_DIRECTORY ${LIBRARY_OUTPUT_PATH}
     RUNTIME_OUTPUT_DIRECTORY ${EXECUTABLE_OUTPUT_PATH}
-    INSTALL_NAME_DIR lib
+#    INSTALL_NAME_DIR lib
   )
 
   # For dynamic link numbering convenions
